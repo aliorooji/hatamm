49c44fbd10da50551c138d7a04cfda91a77d0837
aliorooji <ali.orooji.ilia@gmail.com>
2017-07-18 09:41:30 +0430

    c

--- NotAndBlog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotandblogConfig(AppConfig):
-    name = 'NotAndBlog'
--- NotAndBlog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from . import views, ajax, ajax_post
-from django.views.generic.base import TemplateView
-
-urlpatterns = [
-    url(r'^ajax/post/report_this$', ajax_post.report_this, name='report_this'),
-]
